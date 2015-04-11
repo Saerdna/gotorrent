bfd82fea6b96ad0f015db957c39addafb2a789c9
Austin McDonald <apm@google.com>
2015-04-11 17:41:46 -0400

    reflection-based bencoding, first pass

--- bencoding.go
+++ bencoding.go
@@ -15,8 +15,36 @@ func Marshal(v interface{}) (string, error) {
 	case reflect.String:
 		s := v.(string)
 		return strconv.Itoa(len(s)) + ":" + s, nil
+	case reflect.Struct:
+		l := "l"
+		for i := 0; i < value.NumField(); i++ {
+			field := value.Field(i)
+			if field.CanInterface() {
+				s, err := Marshal(field.Interface())
+				if err != nil {
+					return "", err
+				}
+				l += s
+			}
+		}
+		l += "e"
+		return l, nil
+	case reflect.Map:
+		d := "d"
+		for _, key := range value.MapKeys() {
+			ks, err := Marshal(key.Interface())
+			if err != nil {
+				return "", err
+			}
+			vs, err := Marshal(value.MapIndex(key).Interface())
+			if err != nil {
+				return "", err
+			}
+			d += ks + vs
+		}
+		d += "e"
+		return d, nil
 	default:
-		return "", fmt.Errorf("Unknown type for object %v", v)
+		return "", fmt.Errorf("Can't marshal type %v, value %v", value.Kind(), v)
 	}
-	return "", nil
 }
--- bencoding_test.go
+++ bencoding_test.go
@@ -2,19 +2,32 @@ package gotorrent
 
 import "testing"
 
+type TestList struct {
+	Name string
+	Age  int
+}
+
+func ValidateMarshal(input interface{}, expected string, t *testing.T) {
+	actual, err := Marshal(input)
+	if err != nil {
+		t.Errorf("Error marshalling %v (expected %v, received %v): %v",
+			input, expected, actual, err)
+	}
+	if expected != actual {
+		t.Errorf("Expected %v, got %v on input %v", expected, actual, input)
+	}
+}
+
 func TestMarshal(t *testing.T) {
 	marshalData := map[interface{}]string{
-		10:     "i10e",
-		"spam": "4:spam",
+		10:                    "i10e",
+		"spam":                "4:spam",
+		TestList{"alice", 30}: "l5:alicei30ee",
 	}
 	for input, expected := range marshalData {
-		actual, err := Marshal(input)
-		if err != nil {
-			t.Errorf("Error marshalling %v (expected %v, received %v): %v",
-				input, expected, actual, err)
-		}
-		if expected != actual {
-			t.Errorf("Expected %v, got %v on input %v", expected, actual, input)
-		}
+		ValidateMarshal(input, expected, t)
 	}
+	ValidateMarshal(
+		map[TestList]int{TestList{"bob", 25}: 30, TestList{"alice", 30}: 35},
+		"dl3:bobi25eei30el5:alicei30eei35ee", t)
 }
