e7abd8a37c2609ca234b6758fa505bd783f3fc33
Austin McDonald <apm@google.com>
2015-04-11 19:26:53 -0400

    easy unmarshalling

--- bencoding.go
+++ bencoding.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 func Marshal(v interface{}) (string, error) {
@@ -29,8 +30,20 @@ func Marshal(v interface{}) (string, error) {
 		}
 		l += "e"
 		return l, nil
+	case reflect.Array, reflect.Slice:
+		l := "l"
+		for i := 0; i < value.Len(); i++ {
+			s, err := Marshal(value.Index(i).Interface())
+			if err != nil {
+				return "", err
+			}
+			l += s
+		}
+		l += "e"
+		return l, nil
 	case reflect.Map:
 		d := "d"
+		// TODO(apm): The iteration order is nondeterministic.  Ugh.
 		for _, key := range value.MapKeys() {
 			ks, err := Marshal(key.Interface())
 			if err != nil {
@@ -44,18 +57,51 @@ func Marshal(v interface{}) (string, error) {
 		}
 		d += "e"
 		return d, nil
-	case reflect.Array, reflect.Slice:
-		l := "l"
-		for i := 0; i < value.Len(); i++ {
-			s, err := Marshal(value.Index(i).Interface())
-			if err != nil {
-				return "", err
-			}
-			l += s
-		}
-		l += "e"
-		return l, nil
 	default:
 		return "", fmt.Errorf("Can't marshal type %v, value %v", value.Kind(), v)
+	}
+}
+
+func Unmarshal(s string, v interface{}) error {
+	ptr_value := reflect.ValueOf(v)
+	switch ptr_value.Kind() {
+	case reflect.Interface, reflect.Ptr:
+		break
+	default:
+		return fmt.Errorf("Must pass a pointer or struct to Unmarshal, received %v", ptr_value)
+	}
+
+	value := ptr_value.Elem()
+	if !value.CanSet() {
+		return fmt.Errorf("Received unsettable value %v", v)
+	}
+	switch value.Kind() {
+	case reflect.Int:
+		if s[0] != 'i' || s[len(s)-1] != 'e' {
+			return fmt.Errorf("Expected integer for %v, found %v", v, s)
+		}
+		s = s[1 : len(s)-1]
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return fmt.Errorf("Error parsing %v as int: %v", s, err)
+		}
+		value.SetInt(int64(i))
+		return nil
+	case reflect.String:
+		substrings := strings.SplitN(s, ":", 2)
+		if len(substrings) < 2 {
+			return fmt.Errorf("Invalid string, missing colon: %v", s)
+		}
+		length, err := strconv.Atoi(substrings[0])
+		if err != nil {
+			return fmt.Errorf("Unable to parse length for string %v: err %v", s, err)
+		}
+		if len(substrings[1]) != length {
+			return fmt.Errorf("Invalid length specification for string %v", s)
+		}
+		value.SetString(substrings[1])
+		return nil
+	default:
+		return fmt.Errorf("Can't unmarshal to type %v, value %v", value.Kind(), v)
 	}
 }
--- bencoding_test.go
+++ bencoding_test.go
@@ -12,9 +12,11 @@ func ValidateMarshal(input interface{}, expected string, t *testing.T) {
 	if err != nil {
 		t.Errorf("Error marshalling %v (expected %v, received %v): %v",
 			input, expected, actual, err)
+		return
 	}
 	if expected != actual {
 		t.Errorf("Expected %v, got %v on input %v", expected, actual, input)
+		return
 	}
 }
 
@@ -31,4 +33,32 @@ func TestMarshal(t *testing.T) {
 		map[TestList]int{TestList{"bob", 25}: 30, TestList{"alice", 30}: 35},
 		"dl3:bobi25eei30el5:alicei30eei35ee", t)
 	ValidateMarshal([]int{10, 20, 30}, "li10ei20ei30ee", t)
+}
+
+func ValidateUnmarshal(input string, expected, actual interface{}, err error, t *testing.T) {
+	if err != nil {
+		t.Errorf("Error marshalling %v (expected %v, received %v): %v",
+			input, expected, actual, err)
+		return
+	}
+	if expected != actual {
+		t.Errorf("Expected %v, got %v on input %v", expected, actual, input)
+		return
+	}
+}
+
+func TestUnmarshalInt(t *testing.T) {
+	var actual int
+	input := "i10e"
+	expected := 10
+	err := Unmarshal(input, &actual)
+	ValidateUnmarshal(input, expected, actual, err, t)
+}
+
+func TestUnmarshalString(t *testing.T) {
+	var actual string
+	input := "4:spam"
+	expected := "spam"
+	err := Unmarshal(input, &actual)
+	ValidateUnmarshal(input, expected, actual, err, t)
 }
