149ee1ba628e539546bdc1df9b6234d0922d395d
Austin McDonald <apm@google.com>
2015-04-12 12:02:41 -0400

    tracker query

--- tracker.go
+++ tracker.go
@@ -2,8 +2,10 @@ package gotorrent
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 const peerID = "G001-----ABCDEFGHIJK"
@@ -17,17 +19,29 @@ func QueryTracker(metaInfo MetaInfo) error {
 	values.Set("peer_id", peerID)
 	values.Set("port", "6881")
 	values.Set("info_hash", metaInfo.InfoHash)
+	values.Set("uploaded", "0")
+	values.Set("downloaded", "0")
+	values.Set("left", strconv.Itoa(metaInfo.Info.Length))
+	values.Set("compact", "0")
+	values.Set("no_peer_id", "0")
+	// values.Set("event", "started")
 	tracker_url.RawQuery = values.Encode()
+
 	fmt.Printf("querying %v\n", tracker_url)
 	resp, err := http.Get(tracker_url.String())
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+	fmt.Println(resp)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("Non-200 response: %v", resp)
 	}
-	fmt.Println(resp)
 
 	return nil
 }
