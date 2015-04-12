9a19d138519ebf78fc0d275b5ff5f4dee7ac3b6b
Austin McDonald <apm@google.com>
2015-04-12 18:31:11 -0400

    first pass at a parser

--- bencoding/bencoding.go
+++ bencoding/bencoding.go
@@ -1,35 +1,216 @@
 package bencoding
 
 import (
-	"bytes"
+	"bufio"
 	"strconv"
 )
 
-func EncodeInt(i int, writer bytes.Buffer) {
-	writer.WriteString("i")
-	writer.WriteString(strconv.Itoa(i))
-	writer.WriteString("e")
-}
+type Token int
 
-func EncodeString(s string, writer bytes.Buffer) {
-	writer.WriteString(strconv.Itoa(len(s)))
-	writer.WriteString(":")
-	writer.WriteString(s)
-}
+const (
+	EOF Token = iota
+	LIST_START
+	DICT_START
+	END
+	INT
+	STRING
+	ILLEGAL
+)
 
-func EncodeList(l []string, writer bytes.Buffer) {
-	writer.WriteString("l")
-	for _, element := range l {
-		writer.WriteString(element)
+func NextToken(b *bufio.Reader) (token Token, value string) {
+	r, _, err := b.ReadRune()
+	if err != nil {
+		return EOF, ""
 	}
-	writer.WriteString("e")
+	switch {
+	case r == 'l':
+		return LIST_START, ""
+	case r == 'd':
+		return DICT_START, ""
+	case r == 'e':
+		return END, ""
+	case r == 'i':
+		i, err := b.ReadString('e')
+		if err != nil {
+			return EOF, ""
+		}
+		return INT, i[0 : len(i)-1]
+	case '0' <= r && r <= '9':
+		b.UnreadRune()
+		length_string, err := b.ReadString(':')
+		if err != nil {
+			return EOF, ""
+		}
+
+		length, err := strconv.Atoi(length_string[:len(length_string)-1])
+		if err != nil {
+			return ILLEGAL, ""
+		}
+
+		buffer := make([]byte, length)
+		n, err := b.Read(buffer)
+		if err != nil || n != length {
+			return EOF, ""
+		}
+		return STRING, string(buffer)
+	default:
+		return ILLEGAL, ""
+	}
 }
 
-func EncodeDict(m map[string]string, writer bytes.Buffer) {
-	writer.WriteString("d")
-	for key, value := range m {
-		writer.WriteString(key)
-		writer.WriteString(value)
-	}
-	writer.WriteString("e")
-}
+// type Node interface {
+// 	Decode(*bufio.Reader) error
+// }
+
+// type Int struct {
+// 	Value int
+// }
+
+// type String struct {
+// 	Value string
+// }
+
+// type List struct {
+// 	Value []Node
+// }
+
+// type End struct{}
+
+// func DecodeUnknown(b *bufio.Reader) (Node, error) {
+// 	start, err := b.Peek(1)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	for {
+// 		switch string(start) {
+// 		case "i":
+// 			var value Int
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return value, err
+// 			}
+// 			return value, nil
+// 		case "l":
+// 			var value List
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return value, err
+// 			}
+// 			return value, nil
+// 		case "d":
+// 			var value Dict
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return value, err
+// 			}
+// 			return value, nil
+// 		case "e":
+// 			return End{}, nil
+// 		default:
+// 			var value String
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return value, err
+// 			}
+// 			return value, nil
+// 		}
+// 	}
+// }
+
+// func (i *Int) Decode(b *bufio.Reader) error {
+// 	start, err := b.ReadByte()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if start != 'i' {
+// 		return fmt.Errorf("Int didn't start with i.")
+// 	}
+
+// 	s, err := b.ReadString('e')
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	i.Value, err = strconv.Atoi(s[:len(s)-1])
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+// func (s *String) Decode(b *bufio.Reader) error {
+// 	length_string, err := b.ReadString(':')
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	length, err := strconv.Atoi(length_string[:len(length_string)-1])
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	buffer := make([]byte, length)
+// 	n, err := b.Read(buffer)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if n != length {
+// 		return fmt.Errorf("String had length %v, but buffer only had %v bytes: %v",
+// 			length, len(s.Value), s.Value)
+// 	}
+
+// 	s.Value = string(buffer)
+// 	return nil
+// }
+
+// func (l *List) Decode(b *bufio.Reader) error {
+// 	start := make([]byte, 1)
+// 	n, err := b.Read(start)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if n < len(start) || string(start) != "l" {
+// 		return fmt.Errorf("Invalid list.")
+// 	}
+
+// 	start, err = b.Peek(1)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	for {
+// 		switch string(start) {
+// 		case "i":
+// 			var value Int
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			l.Value = append(l.Value, value)
+// 		case "l":
+// 			var value List
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			l.Value = append(l.Value, value)
+// 		case "d":
+// 			var value Dict
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			l.Value = append(l.Value, value)
+// 		default:
+// 			var value String
+// 			err = value.Decode(r)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			l.Value = append(l.Value, value)
+// 		}
+// 	}
+// }
+
+// func (e *End) Decode(b *bufio.Reader) error {
+
+// }
--- bencoding/bencoding_test.go
+++ bencoding/bencoding_test.go
@@ -1,23 +1,76 @@
 package bencoding
 
 import (
-	"bytes"
+	"bufio"
+	"strings"
 	"testing"
 )
 
-func TestEncodeInt(t *testing.T) {
-	testData := map[int]string{
-		10:    "i10e",
-		0:     "i0e",
-		12345: "i12345e",
-		-100:  "i-100e",
+func TestParser(t *testing.T) {
+	input := "d4:name5:alice4:kidsl3:bob5:carole3:agei30ee"
+	expectedTokens := []Token{
+		DICT_START,
+		STRING,
+		STRING,
+		STRING,
+		LIST_START,
+		STRING,
+		STRING,
+		END,
+		STRING,
+		INT,
+		END,
 	}
-	for input, expected := range testData {
-		var buffer bytes.Buffer
-		EncodeInt(input, buffer)
-		actual := buffer.String()
-		if expected != actual {
-			t.Errorf("Expected: %v Actual: %v\n", expected, actual)
+	expectedValues := []string{
+		"",
+		"name",
+		"alice",
+		"kids",
+		"",
+		"bob",
+		"carol",
+		"",
+		"age",
+		"30",
+		"",
+	}
+	reader := bufio.NewReader(strings.NewReader(input))
+	for i, expectedToken := range expectedTokens {
+		expectedValue := expectedValues[i]
+		actualToken, actualValue := NextToken(reader)
+		if actualToken != expectedToken {
+			t.Errorf("Expected %v, Actual %v", expectedToken, actualToken)
+		}
+		if actualValue != expectedValue {
+			t.Errorf("Expected %v, Actual %v", expectedValue, actualValue)
 		}
 	}
 }
+
+// func TestDecodeInt(t *testing.T) {
+// 	actual := Int{}
+// 	input := "i10e"
+// 	expected := 10
+// 	err := actual.Decode(strings.NewReader(input))
+// 	if err != nil {
+// 		t.Errorf("Error decoding %v %v", input, err)
+// 	}
+// 	if actual.Value != expected {
+// 		t.Errorf("Results don't match when decoding %v.  Expected %v Actual %v",
+// 			input, expected, actual)
+// 	}
+// }
+
+// func TestDecodeString(t *testing.T) {
+// 	actual := String{}
+// 	input := "4:spam"
+// 	expected := "spam"
+// 	err := actual.Decode(strings.NewReader(input))
+// 	if err != nil {
+// 		t.Errorf("Error decoding %v %v", input, err)
+// 	}
+// 	if actual.Value != expected {
+// 		t.Errorf("Results don't match when decoding %v.  Expected %v Actual %v",
+// 			input, expected, actual)
+// 	}
+// }
